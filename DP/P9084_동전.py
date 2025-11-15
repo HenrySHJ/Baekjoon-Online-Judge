b48fb07d32aaabb703dfbeff8302d02bc4201688
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2025-11-15 17:18:56 +0900

    Add files via upload

---
+++ DP/P9084_동전.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    coins = list(map(int,input().split()))
+    M = int(input())
+
+    DP = [0]*(M+1)
+    DP[0] = 1
+    for coin in coins:
+        for i in range(coin,M+1):
+            DP[i] += DP[i-coin]
+
+    print(DP[M])
