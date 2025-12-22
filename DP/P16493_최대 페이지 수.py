1f6b41f389a54a05b9cf3b8e88cb59fdb4a9e9d7
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2025-12-22 14:40:35 +0900

    Add files via upload

--- DP/P16493_최대 페이지 수.py
+++ DP/P16493_최대 페이지 수.py
@@ -3,11 +3,18 @@ input = sys.stdin.readline
 
 N,M = map(int,input().split())
 
-chap = [[0,0] for _ in range(N+1)]
+chap = [[0,0] for _ in range(M+1)]
 
 for i in range(1,M+1):
     d,p = map(int,input().split())
     chap[i] = [d,p]
 
+chap.sort(key = lambda x:x[0])
 # DP[i] : i일 동안 읽을 수 있는 최대 페이지 수
 DP = [0]*(N+1)
+
+for d,p in chap:              
+    for day in range(N, d-1, -1):
+        DP[day] = max(DP[day], DP[day-d] + p)
+
+print(DP[N])
