31d392e34bcc1595a5e3e30f254b64fea2afebb5
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-01-10 16:38:52 +0900

    Add files via upload

---
+++ DataStructure/P10814_나이순 정렬.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+info = []
+for _ in range(N):
+    age,name = input().split()
+    info.append((int(age),name))
+
+info.sort(key=lambda x:x[0])
+
+for age,name in info:
+    print(age,name)
