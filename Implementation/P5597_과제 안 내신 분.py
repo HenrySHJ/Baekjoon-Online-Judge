0704ce36092df4f628f73d03b99a40a2ee1f19ad
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-01-13 14:16:14 +0900

    Add files via upload

---
+++ Implementation/P5597_과제 안 내신 분.py
@@ -0,0 +1,13 @@
+check = [False]*31
+
+for i in range(28):
+    n = int(input())
+    check[n] = True
+
+count = 0
+for i in range(1,31):
+    if count == 2:
+        break
+    if not check[i]:
+        print(i)
+        count += 1
