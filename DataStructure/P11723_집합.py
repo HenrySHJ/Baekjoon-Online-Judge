fe7735e2a99bc80f1436f8abda2709b134063328
hyunjongsuh <222944122+HenrySHJ@users.noreply.github.com>
2026-02-02 14:55:38 +0900

    Add files via upload

---
+++ DataStructure/P11723_집합.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+S = set()
+
+for _ in range(M):
+    check = list(input().split())
+
+    if check[0] == 'add':
+        S.add(int(check[1]))
+
+    elif check[0] == 'remove':
+        S.remove(int(check[1]))
+
+    elif check[0] == 'check':
+        if int(check[1]) in S:
+            print(1)
+        else:
+            print(0)
+
+    elif check[0] == 'toggle':
+        if int(check[1]) in S:
+            S.remove(int(check[1]))
+        else:
+            S.add(int(check[1]))
+
+    elif check[0] == 'all':
+        S = {i for i in range(1,21)}
+
+    elif check[0] == 'empty':
+        S = set()
